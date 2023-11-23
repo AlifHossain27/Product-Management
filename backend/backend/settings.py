2abc8615ca059b5359252090b25a06d69f6d02ea
AlifHossain27 <alifh044@gmail.com>
2023-11-23 17:08:57 +0600

    connecting to mysql database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,11 +83,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": env("DB_INFO_PATH"),
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

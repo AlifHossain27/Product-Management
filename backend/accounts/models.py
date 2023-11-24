e20058462fb58ef99b7a7aabf95d6e10f6f9496f
AlifHossain27 <alifh044@gmail.com>
2023-11-24 07:16:17 +0600

    Adding User model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=225, unique=True)
+    email = models.EmailField(max_length=225, unique=True)
+    password = models.CharField(max_length=225)
+    date_joined = models.DateField(auto_now_add=True)
+    last_login = models.DateField(auto_now=True)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = []

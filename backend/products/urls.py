bb887273b82c88156c90310cd85801d9aed8879a
AlifHossain27 <alifh044@gmail.com>
2023-12-08 11:01:03 +0600

    Adding Products endpoints

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import ProductCreateListAPI, ProductRetrieveUpdateDeleteAPI
 
 urlpatterns = [
-    
+    path("product/", ProductCreateListAPI.as_view(), name = "product"),
+    path("product/<int:product_id>", ProductRetrieveUpdateDeleteAPI.as_view(), name = "product_detail")
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework import views, response, permissions, status
+from user import authentication
+from .serializers import ProductSerializer
+from . import services
 
-# Create your views here.
+class ProductCreateListAPI(views.APIView):
+    authentication_classes = (authentication.CustomUserAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    # Create a new product
+    def post(self, request):
+        serializer = ProductSerializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        data = serializer.validated_data
+        serializer.instance = services.create_product(user = request.user, product = data)
+        
+        return response.Response(data = serializer.data)
+
+    # Retrives a list of products
+    def get(self, request):
+        product_collection = services.get_user_products(user = request.user)
+        serializer = ProductSerializer(product_collection, many = True)
+        
+        return response.Response(data = serializer.data)
+    
+class ProductRetrieveUpdateDeleteAPI(views.APIView):
+    authentication_classes = (authentication.CustomUserAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    # Retrieves a specific product
+    def get(self, request, product_id):
+        product = services.get_user_product_detail(product_id=product_id)
+        serializer = ProductSerializer(product)
+        
+        return response.Response(data=serializer.data)
+    
+    # Deletes a specific product
+    def delete(self, request, product_id):
+        services.delete_user_product(user = request.user, product_id=product_id)
+        
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
+
+    # Updates a specific product
+    def put(self, request, product_id):
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        product = serializer.validated_data
+        serializer.instance = services.update_user_product(user = request.user, product_id=product_id, product_data = product)
+
+        return response.Response(data = serializer.data)
